14046957361185294987b02002542da7765790d4
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-03-13 19:37:30 +0530

    update

--- build/CMakeFiles/SpaceInvaders.dir/src/main.cpp.o.d
+++ build/CMakeFiles/SpaceInvaders.dir/src/main.cpp.o.d
@@ -201,4 +201,46 @@ CMakeFiles/SpaceInvaders.dir/src/main.cpp.o: \
   /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__type_traits/result_of.h \
   /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__functional/invoke.h \
   /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__type_traits/type_identity.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__type_traits/unwrap_ref.h
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__type_traits/unwrap_ref.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/new \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/__exception/exception.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/stdlib.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/stdlib.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/_stdlib.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/wait.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_pid_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_id_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/signal.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/appleapiopts.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/machine/signal.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/arm/signal.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/machine/_mcontext.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/arm/_mcontext.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/mach/machine/_structs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/mach/arm/_structs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_pthread/_pthread_attr_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_sigaltstack.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_ucontext.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_sigset_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_uid_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/resource.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_timeval.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/machine/endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/arm/endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/machine/_endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/arm/_endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/__endian.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/libkern/_OSByteOrder.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/libkern/arm/_OSByteOrder.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/alloca.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_ct_rune_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_rune_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_wchar_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/malloc/_malloc.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/malloc/_malloc_type.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/malloc/_ptrcheck.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/_abort.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_dev_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_types/_mode_t.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/c++/v1/cstdlib
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,64 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <cstdint>
 #include <cstdio>
+#include <limits>
+#include <new>
 
 void error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
 }
 
+struct Buffer {
+  size_t width, height;
+  // represents each pixel as uint32_t
+  uint32_t *data; // stores 4 8-bit color values, for each pixel
+};
+
+// clear the buffer to a certain color:
+void buffer_clear(Buffer *buffer, uint32_t color) {
+  for (size_t i = 0; i < buffer->width * buffer->height; ++i) {
+    buffer->data[i] = color;
+  }
+}
+
+// define colors as uint32_t values
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b) {
+  return (r << 24) | (g << 16) | (b << 8) | 255;
+}
+
+void validate_shader(GLuint shader, const char *file = 0) {
+  static const unsigned int BUFFER_SIZE = 512;
+  char buffer[BUFFER_SIZE];
+  GLsizei length = 0;
+
+  glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+  if (length > 0) {
+    printf("Shader %d(%s) compile error: %s\n", shader, (file ? file : ""),
+           buffer);
+  }
+}
+
+bool validate_program(GLuint program) {
+  static const GLsizei BUFFER_SIZE = 512;
+  GLchar buffer[BUFFER_SIZE];
+  GLsizei length = 0;
+
+  glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+
+  if (length > 0) {
+    printf("Program %d link error: %s\n", program, buffer);
+    return false;
+  }
+
+  return true;
+}
+
 int main(int argc, char *argv[]) {
+  const size_t buffer_width = 224;
+  const size_t buffer_height = 256;
+
   // initialzing glfw library
   if (!glfwInit())
     return -1;
@@ -18,11 +70,10 @@ int main(int argc, char *argv[]) {
     glfwTerminate();
     return -1;
   }
-  //make subsequent opengl calls
+  // make subsequent opengl calls
   glfwMakeContextCurrent(window);
 
-
-  //initilaize glew, a loading library
+  // initilaize glew, a loading library
   GLenum err = glewInit();
   if (err != GLEW_OK) {
     fprintf(stderr, "Error initializing GLEW.\n");
@@ -36,6 +87,104 @@ int main(int argc, char *argv[]) {
 
   printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
 
+  uint32_t clear_color = rgb_to_uint32(0, 128, 0);
+  Buffer buffer;
+  buffer.width = buffer_width;
+  buffer.height = buffer_height;
+  buffer.data = new (std::nothrow) uint32_t[buffer.width * buffer.height];
+  if (!buffer.data) {
+    fprintf(stderr, "Failed to allocate memory for buffer.\n");
+    return -1;
+  }
+
+  buffer_clear(&buffer, clear_color);
+
+  // create texture for presenting buffer to OpenGL
+  GLuint buffer_texture;
+  glGenTextures(1, &buffer_texture);
+  glBindTexture(GL_TEXTURE_2D, buffer_texture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, buffer.width, buffer.height, 0,
+               GL_RGBA, GL_UNSIGNED_BYTE, buffer.data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  GLuint fullscreen_triangle_vao;
+  glGenVertexArrays(1, &fullscreen_triangle_vao);
+  glBindVertexArray(fullscreen_triangle_vao);
+
+  // Create shader for displaying buffer
+  static const char *fragment_shader =
+      "\n"
+      "#version 330\n"
+      "\n"
+      "uniform sampler2D buffer;\n"
+      "noperspective in vec2 TexCoord;\n"
+      "\n"
+      "out vec3 outColor;\n"
+      "\n"
+      "void main(void){\n"
+      "    outColor = texture(buffer, TexCoord).rgb;\n"
+      "}\n";
+
+  static const char *vertex_shader =
+      "\n"
+      "#version 330\n"
+      "\n"
+      "noperspective out vec2 TexCoord;\n"
+      "\n"
+      "void main(void){\n"
+      "\n"
+      "    TexCoord.x = (gl_VertexID == 2)? 2.0: 0.0;\n"
+      "    TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
+      "    \n"
+      "    gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
+      "}\n";
+
+  GLuint shader_id = glCreateProgram();
+
+  // Create vertex shader
+  {
+    GLuint shader_vp = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(shader_vp, 1, &vertex_shader, 0);
+    glCompileShader(shader_vp);
+    validate_shader(shader_vp, vertex_shader);
+    glAttachShader(shader_id, shader_vp);
+
+    glDeleteShader(shader_vp);
+  }
+
+  // Create fragment shader
+  {
+    GLuint shader_fp = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(shader_fp, 1, &fragment_shader, 0);
+    glCompileShader(shader_fp);
+    validate_shader(shader_fp, fragment_shader);
+    glAttachShader(shader_id, shader_fp);
+
+    glDeleteShader(shader_fp);
+  }
+
+  glLinkProgram(shader_id);
+
+  if (!validate_program(shader_id)) {
+    fprintf(stderr, "Error while validating shader.\n");
+    glfwTerminate();
+    glDeleteVertexArrays(1, &fullscreen_triangle_vao);
+    delete[] buffer.data;
+    return -1;
+  }
+
+  GLint location = glGetUniformLocation(shader_id, "buffer");
+  glUniform1i(location, 0);
+
+  glDisable(GL_DEPTH_TEST);
+  glBindVertexArray(fullscreen_triangle_vao);
+
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 
   // game loop:
   // specifies window color, here red
