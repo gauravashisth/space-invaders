98822b8eec716b7fd925cf56d3a4f43a82e2ae0c
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-03-30 12:33:15 +0530

    added active field for laser.

--- src/laser.cpp
+++ src/laser.cpp
@@ -1,13 +1,22 @@
 #include "laser.hpp"
+#include <iostream>
 
 laser::laser(Vector2 position, int speed) {
   // to set the values we get from constructor, to attributes.
   this->position = position;
   this->speed = speed;
+  active = true;
 }
-
 void laser::draw() {
-  DrawRectangle(position.x, position.y, 4, 15, {243, 216, 63, 255});
+  if (active)
+    DrawRectangle(position.x, position.y, 4, 15, {243, 216, 63, 255});
 }
 
-void laser::update() { position.y += speed; }
+void laser::update() {
+  position.y += speed;
+  if (active)
+    if (position.y > GetScreenHeight() || position.y < 0) {
+      active = false;
+      std::cout << "LASER INACTIVE\n";
+    }
+}
--- src/laser.hpp
+++ src/laser.hpp
@@ -6,6 +6,7 @@ public:
   laser(Vector2 position, int speed);
   void update();
   void draw();
+  bool active;
 
 private:
   Vector2 position;
